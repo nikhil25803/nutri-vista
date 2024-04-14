b59ab7b4bf7f126e672dd229d1cf7addccc8918d
Nikhil Raj <nikhil25803@gmail.com>
2024-04-14 18:44:05 +0530

    Entry filed heading change

--- src/app/_components/Profile/EntryField.tsx
+++ src/app/_components/Profile/EntryField.tsx
@@ -79,10 +79,10 @@ export default function EntryField(props: EntryPropsInterface) {
   return (
     <section className="bg-backgroundDark mt-10 my-auto px-2 py-10 rounded-xl">
       <div className="flex flex-col items-center gap-y-5">
-        <div className="text-2xl md:text-3xl">How was your day?</div>
+        <div className="text-2xl md:text-3xl">How was your today's intake?</div>
 
         <textarea
-          placeholder="Your words ..."
+          placeholder="Write here ..."
           className="w-2/3 bg-textGrey px-3 py-5 rounded-xl overflow-y-hidden resize-none"
           value={value}
           onChange={(e) => setValue(e.target.value)}
