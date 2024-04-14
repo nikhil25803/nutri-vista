7e359de1f30123847fe4b285bd20fa51e69e6473
Nikhil Raj <nikhil25803@gmail.com>
2024-04-14 07:34:54 +0530

    Deployment Issue Fixed

---
+++ src/utills/authOptions.ts
@@ -0,0 +1,41 @@
+import { OnboardUserToDB } from "@/app/helpers/userToDB";
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID!,
+        clientSecret: process.env.GOOGLE_SECRET!,
+      }),
+      GithubProvider({
+        clientId: process.env.GITHUB_ID!,
+        clientSecret: process.env.GITHUB_SECRET!,
+      }),
+    ],
+    callbacks: {
+      async session({ session }) {
+        return session;
+      },
+      async signIn({ profile }) {
+        try {
+          // Onboard user to DB
+          const onboardUser = await OnboardUserToDB(profile);
+  
+          // If user has been added to DB successfully or not.
+          if (onboardUser) {
+            return true;
+          }
+          return false;
+        } catch (error) {
+          return false;
+        }
+      },
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+      signIn: "/auth",
+    },
+  };
